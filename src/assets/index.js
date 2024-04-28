562cfb877d9bda99cfc3af74352ffbd09c4746bc
Richa Sinha <96824147+RichaSinha01@users.noreply.github.com>
2024-04-28 23:20:35 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,68 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import java from "./tech/java.png";
+import cplus from "./tech/cplus.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import c from "./tech/c.png";
+import python from "./tech/python.png";
+import reactjs from "./tech/reactjs.png";
+import sql from "./tech/sql.png";
+import linux from "./tech/linux.png";
+import github1 from "./tech/github1.png";
+import nodejs from "./tech/nodejs.png";
+import aws from "./tech/aws.png";
+import redux from "./tech/redux.png";
+
+
+
+import nss from "./company/nss.png";
+import research from "./company/research.png";
+
+import lung from "./lung.png";
+import multiuser from "./multiuser.png";
+import game from "./game.png";
+import tictac from "./tictac.png";
+import crypto from "./crypto.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  java,
+  cplus,
+  git,
+  html,
+  javascript,
+  c,
+  python,
+  reactjs,
+  nodejs,
+  redux,
+  github1,
+  aws,
+  sql,
+  linux,
+  nss,
+  research,
+  lung,
+  multiuser,
+  game,
+  tictac,
+  crypto,
+};
